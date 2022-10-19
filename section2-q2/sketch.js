4d06b6b1a636f51cce15fe8f4ed5c38b808d7fad
AkariKawaharada <akari_whee_0102@icloud.com>
2022-10-19 11:32:21 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -7,11 +7,18 @@ function setup() {
   for(let i = 0; i < 8; i++){
    for(let j = 0; j < 8; j++){
     if((i+j)%2==1){
-      fill(180);
+      fill(150);
       rect(25*i,25*j,25,25);
+      if(j<3){
+      fill(255,0,0);
+      ellipse(25*i+25/2,25*j+25/2,20,20);}
+      if(j>4){
+      fill(0);
+      ellipse(25*i+25/2,25*j+25/2,20,20);}
+
       
-    
-    }
+      }
+      
   
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
